19e3881553bcd3a55d33132e7c301ab25b4c22a4
Gregory M. Bice <CosmicSeagullYT@gmail.com>
2018-05-21 21:06:32 -0400

    ngfhf

---
+++ PaymentsFinal/src/main/java/cisc/cisc181/edu/PaymentsFinal/App.java
@@ -0,0 +1,13 @@
+package cisc.cisc181.edu.PaymentsFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
